b55bef8fae8f31145809b5db96e53504ff324332
WiiJoy MacOS <wiijoy@bk.ru>
2022-04-18 14:23:35 +0300

    fix: typos and add devtools img

--- src/components/Main/Prepare/Prepare.jsx
+++ src/components/Main/Prepare/Prepare.jsx
@@ -5,7 +5,7 @@ export default function Prepare() {
     <section className='prepare'>
       <div className='content-wrapper'>
         <h2></h2>
-        <div className='task-description'>Уважаемые проверяющие.<br/>Проверяются только требования, указанные в критериях.<br/>Если какой-то момент в критериях не описан, его не проверяем и не оцениваем.<br/>Если сомневаетесь вычитать баллы или нет, - не вычитайте.<br/>Все сомнения трактуются в пользу студента!<br/>Задание второй недели: https://github.com/rolling-scopes-school/tasks/tree/master/stage1/stream1/shelter#неделя-2</div>
+        <div className='task-description'>Уважаемые проверяющие.<br/>Проверяются только требования, указанные в критериях.<br/>Если какой-то момент в критериях не описан, его не проверяем и не оцениваем.<br/>Если сомневаетесь вычитать баллы или нет, - не вычитайте.<br/>Все сомнения трактуются в пользу студента!<br/>Задание второй недели: https://github.com/rolling-scopes-school/tasks/tree/master/stage1/stream1/shelter#неделя-2<br/><br/><img src="https://media.discordapp.net/attachments/964058295528738907/965560976466403398/unknown.png" alt="devtools" height="250"></img></div>
       </div>
     </section>
   );
